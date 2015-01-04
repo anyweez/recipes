e663e25c84bf52ba7b720267ae57ce1261c83ad2
Luke Segars <luke@lukesegars.com>
2015-01-03 20:26:35 -0800

    Skeleton (non-compilable) algorithm for parsing Freebase file.

--- src/extract/ingredients.go
+++ src/extract/ingredients.go
@@ -10,14 +10,72 @@ import (
 )
 
 /**
+ * This function takes a line of a RDF file and breaks it up into pieces.
+ * It also strips quotation marks, removes angled brackets, etc.
+ */
+func split(line string) (string, string, string) {
+	return "one", "two", "three"
+}
+
+func store(mapping *map[string]bool, subj string, pred string, obj string) {
+	// This switch statement identifies which predicates should be stored,
+	// and what field they should be stored in.
+	switch (pred) {
+		case "/name":
+			mapping[subj].Name = obj
+			break
+		default:
+			break
+	}
+}
+
+/**
+ * Checks whether a given tuple invalidates the subject. Note that the
+ * subject only needs to be invalidated (marked non-keeper) once in order
+ * to be skipped.
+ */
+func isKeeper(subj, pred, obj string) bool {
+	return false
+}
+
+/**
  * This function parses a Freebase archive and generates a list of
  * Ingredient structures.
  */
 func ExtractIngredients(conf config.RecipesConfig) []proto.Ingredient {
 	ingredients := make([]proto.Ingredient, 0)
 	
-	ingredients = append(ingredients, proto.Ingredient{ Name: gproto.String("tater salad") })
-	ingredients = append(ingredients, proto.Ingredient{ Name: gproto.String("cole slaw") })
+	// Step 1: open file w/ reader (note that it can be VERY big so it needs to be buffered)
+	
+	// Step 2: create two maps, one that keeps track of ingredient data (keyed by mid)
+	//   and the other that keeps track of whether a given ingredient is a "keeper."
+	//   Note that all ingredients are assuming to be keepers until a property is
+	//   discovered that makes them no longer interesting (such as being of the wrong
+	//   type).
+	im := make(map[string]proto.Ingredient)
+	iv := make(map[string]bool)
+	
+##	for _, line := line of freebase file {
+		subj, pred, obj := split(line)
+		
+		// Check whether this record invalidates the subject.
+		if !isKeeper(subj, pred, obj) {
+			iv[subj] = false
+##			delete im[subj]
+		} else {
+			// If not, let's see if its a fact that needs to be saved.
+			valid, exists := iv[subj]
+			// Save the record if so, otherwise just skip it.
+			if exists && valid {
+				store(&im, subj, pred, obj)
+			}
+		}			
+	}
+	
+	// Convert the map into a slice that can be returned.
+	for _, ingr := range im {
+		ingredients = append(ingredients, ingr)
+	}
 	
 	return ingredients
 }

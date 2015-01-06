25b7fb228e7b711657377eedf188592246999e0d
Luke Segars <luke@lukesegars.com>
2015-01-05 23:46:52 -0800

    Added support for gcfg.

--- src/lib/config/config.go
+++ src/lib/config/config.go
@@ -2,28 +2,33 @@ package config
 
 import (
 	"fmt"
+	gcfg "code.google.com/p/gcfg"
 )
 
 type RecipesConfig struct {
-	MongoAddress 				string
-	MongoPort    				int
-	FreebaseDump				string
-	MongoDatabase				string
-	MongoRawCollection			string
-	MongoRecipeCollection		string
-	MongoIngredientCollection	string	
+	Mongo		MongoConfig
+	Freebase	FreebaseConfig
+}
+
+type MongoConfig struct {
+	Address					string
+	Port					string
+	DatabaseName			string
+	RawCollection			string
+	RecipeCollection		string
+	IngredientCollection	string
+}
+
+type FreebaseConfig struct {
+	DumpLocation	string
 }
 
 func New(filename string) RecipesConfig {
-	return RecipesConfig{
-		MongoAddress: 				"historian",
-		MongoPort:    				27017,
-		MongoDatabase:				"recipes",
-		MongoRawCollection:			"scraped",
-		MongoRecipeCollection:		"recipes",
-		MongoIngredientCollection:	"ingredients",	
-		FreebaseDump:				"/mnt/vortex/corpora/freebase/freebase.all",
-	}
+	c := RecipesConfig{}
+	// Read the configuration.
+	gcfg.ReadFileInto(&c, filename)
+
+	return c
 }
 
 func (c *RecipesConfig) Mongo() string {

cff7035f3bbbe8e5049a008b6f70490f83d3efa6
Luke Segars <luke@lukesegars.com>
2015-01-10 23:26:27 -0800

    Added a URL param to /api/best called `seed` that specifies an integer to use as the random seed for the recipes that are returned.

--- src/frontend/best_recipes.go
+++ src/frontend/best_recipes.go
@@ -5,8 +5,11 @@ import (
 	"log"
 	"net/http"
 	"net/rpc"
+	"math/rand"
 	"encoding/json"
 	proto "proto"
+	"strconv"
+	"time"
 )
 
 func best_recipes(w http.ResponseWriter, r *http.Request) {
@@ -14,11 +17,22 @@ func best_recipes(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal("Couldn't connect to retriever: " + err.Error())
 	}
+	
+	rand.Seed(time.Now().Unix())
+	seed := int64(rand.Int())
+
+	rseed_str := r.URL.Query().Get("seed")
+	rseed, err := strconv.ParseInt(rseed_str, 10, 64)
+	
+	if err == nil {
+		seed = rseed
+	}
+
+	log.Println( fmt.Sprintf("Seed: %d", seed) )
 
 	recipes := make([]proto.Recipe, 0)
-   	err = client.Call("Retriever.GetBestRecipes", 12, &recipes)
+   	err = client.Call("Retriever.GetBestRecipes", seed, &recipes)
    	
 	data, _ := json.Marshal(recipes)
-
 	fmt.Fprintf(w, string(data))
 }
